867077f67ad5b02232d71de02e00020d4002b98d
Andrii <A.I.Khmelovsky@gmail.com>
2019-08-01 09:36:37 +0300

    initial digital clock

---
+++ app/scripts.js
@@ -0,0 +1,25 @@
+const clockWrap = document.querySelector('.clock');
+
+const timer = () => {
+
+    const now = new Date();
+    
+    const h = now.getHours();
+    const m = now.getMinutes();
+    const s = now.getSeconds();
+   
+    //console.log(h +':' + m +':' + s)
+    const html = `
+        <span>${h}:</span>
+        <span>${m}:</span>
+        <span>${s}</span>
+        `;
+
+        clockWrap.innerHTML = html;
+};
+
+
+
+setInterval(timer,1000);
+
+
